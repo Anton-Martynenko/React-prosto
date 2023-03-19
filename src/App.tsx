d8354845fecb4e75d7678b0061e9c8c412e95df8
Молодец 2705 <ant_papa@mail.ru>
2023-03-19 15:46:34 +0600

    Rating

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Hello, world!
-    </div>
-  );
+    return (
+        <div>
+            Hello, world!
+            <Rating/>
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>starr</div>
+            <div>starr</div>
+            <div>starr</div>
+            <div>starr</div>
+            <div>starr</div>
+        </div>
+    );
 }
 
 export default App;
