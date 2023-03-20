1a62e5eda8b0978674fd5c3a5ce97a6f716d0462
Молодец 2705 <ant_papa@mail.ru>
2023-03-20 12:54:26 +0600

    PageTitle

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,24 @@ function App() {
     console.log("App rendering");
     return (
         <div>
-            <input type='date'/>
-            <input value='yo' type={'password'}/>
-            <input value='yo'/>
-            <AppTitle />
-            <Rating />
+            <PageTitle title={"Hello world!"}/>
+            <PageTitle title={"Hello my friends!"}/>
+            <Rating value={3}/>
             <Accordion />
-            <Rating />
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+            <Rating value={7}/>
         </div>
     );
 }
 
-function AppTitle() {
+function PageTitle(props: any) {
     console.log("AppTitle rendering");
-    return <>Hello, world!</>
+    return <div>{props.title}</div>
 }
 
 export default App;
