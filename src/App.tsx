c7efe9044503f910c3ff52f5975afaa00caf98c1
Молодец 2705 <ant_papa@mail.ru>
2023-03-20 11:35:14 +0600

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import './App.css';
+import Accordion from "./components/Accordion";
 
 function App() {
     console.log("App rendering");
@@ -29,34 +29,6 @@ function Rating() {
             <Star />
             <Star />
         </div>
-    );
-}
-
-function Accordion() {
-    console.log("Accordion rendering");
-    return (
-        <div>
-            <AccordionTitle />
-            <AccordionBody />
-        </div>
-    );
-}
-
-function AccordionTitle() {
-    console.log("AccordionTitle rendering");
-    return (
-        <h3>Menu</h3>
-    );
-}
-
-function AccordionBody() {
-    console.log("AccordionBody rendering");
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
     );
 }
 
