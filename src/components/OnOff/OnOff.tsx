bcbbee0e82828d2dbb1afe9a97a6b8d890665969
Молодец 2705 <ant_papa@mail.ru>
2023-03-21 20:12:38 +0600

    props.on ?

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -9,18 +9,28 @@ function OnOff(props: PropsType) {
     const onStyle = {
         width: '30px',
         height: '20px',
-        border: '1px solid black'
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: props.on ? 'green' : 'white'
     };
     const offStyle = {
         width: '30px',
         height: '20px',
-        border: '1px solid black'
+        border: '1px solid black',
+        marginLeft: '5px',
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: props.on ? 'white' : 'red'
     };
     const indicatorStyle = {
         width: '30px',
         height: '30px',
         borderRadius: '15px',
-        border: '1px solid black'
+        border: '1px solid black',
+        marginLeft: '5px',
+        display: 'inline-block',
+        backgroundColor: props.on ? 'green' : 'red'
     };
     return (
         <div>
