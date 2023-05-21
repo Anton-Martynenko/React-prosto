59e79f815a65b41f2ce62a25d9540a8b02f90495
Молодец 2705 <ant_papa@mail.ru>
2023-05-21 11:53:22 +0600

    refactor reducer

--- src/components/Accordion/Reducer.tsx
+++ src/components/Accordion/Reducer.tsx
@@ -10,9 +10,10 @@ export const TOGGLE_CONSTANT = "TOGGLE-COLLAPSED";
 export const reducer = (state: StateType, action: ActionType) : StateType => {
     switch (action.type) {
         case TOGGLE_CONSTANT:
-            const stateCopy = {...state};
-            stateCopy.collapsed = !state.collapsed
-            return stateCopy;
+            // const stateCopy = {...state};
+            // stateCopy.collapsed = !state.collapsed
+            // return stateCopy;
+            return {...state, collapsed: !state.collapsed};
         default:
             throw new Error('Bad action type')
     }
