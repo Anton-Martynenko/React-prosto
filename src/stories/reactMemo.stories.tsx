c98303c27b293cffbd3a60fb6304c1aeb76fa346
Молодец 2705 <ant_papa@mail.ru>
2023-05-21 15:10:30 +0600

    RM users

--- src/stories/reactMemo.stories.tsx
+++ src/stories/reactMemo.stories.tsx
@@ -2,4 +2,12 @@ export default {
     title: 'React.memo demo',
 };
 
+export const messageCounter = (props: any) => {
+    return <div>{props.count}</div>
+}
+
+export const Users = (props: {users: Array<string>}) => {
+    return <div>{props.users.map((u, i) => <div>{u}</div>)}</div>
+}
+
 
